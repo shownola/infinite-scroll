fe5bf6ac1fa9aee74f76f82c08d51e588a5e32ea
shownola <swasieko@gmail.com>
2020-09-16 17:06:31 -0700

    Implement functions to pull random images, set attributes to display img titles and links back to unsplash

---
+++ README.md
@@ -0,0 +1,7 @@
+# READNE
+
+Infinite Scrolling is a web design technique that loads content continuously as the user scrolls down the page, eliminating the need for pagination. This application pulls from the unsplash.com api. Unsplash is a free photography sharing site.
+
+Uses a fetch request that contact the Unsplash api server and returns approximately 10 random photos.
+Upon hover of the photo, the title will be displayed.
+Upon clicking the photo, you will be taken to the photo's page on Unsplash's site and in a separate window.
--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-// UNSPLASH api
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
 
+let photosArray = [];
+
+// UNSPLASH api
 const count = 10
 const apiKey = 'AoOR55nJs43JSUQBOwvp9arazEKRQ_AbB3szpSgMWaA';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
+
+
+// Create Elements for Links & Photos, Add to DOM
+function displayPhotos(){
+  photosArray.forEach((photo) => {
+    // Create <a> to link to unsplash
+    const item = document.createElement('a');
+    item.setAttribute('href', photo.links.html);
+    item.setAttribute('target', '_blank');
+    // Create <img> for photo
+    const img = document.createElement('img');
+    img.setAttribute('src', photo.urls.regular);
+    img.setAttribute('alt', photo.alt_description);
+    img.setAttribute('title', photo.alt_description);
+    // Put <img> inside the <a>, then put both inside of the imageContainer element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
 async function getPhotos(){
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
+    photosArray = await response.json();
+    displayPhotos();
   } catch(error){
 
   }
